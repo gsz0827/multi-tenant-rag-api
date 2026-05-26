06df89b1325c246084c3e829bbbbf977e947dd0d
gsz0827 <gzy645888@gmail.com>
2026-05-26 20:34:47 +0800

    switch to PostgreSQL with Alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,15 @@
 import os
 import sys
+from logging.config import fileConfig
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+
+from app.db.session import Base
+from app.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
